3cb04acb57459e944ee4de679ce16782844c54f2
Garrick McCaskill <kiwilifter808@gmail.com>
2017-02-04 16:27:32 +1300

    Add router module, seperate out heroes component from app component.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,38 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {APP_BASE_HREF} from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+	RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+	HeroesComponent
+  ],
+  providers: [
+	HeroService,
+	{provide: APP_BASE_HREF, useValue : '/' }
   ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+
+export class AppModule { 
+
+}
