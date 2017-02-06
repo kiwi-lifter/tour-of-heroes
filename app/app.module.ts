fa993580e30a2b2b84c1bdb095cc8475e83c29f8
Garrick McCaskill <kiwilifter808@gmail.com>
2017-02-06 13:08:17 +1300

    Add dashboard hero selection to navigate to selected hero details.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {APP_BASE_HREF} from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
